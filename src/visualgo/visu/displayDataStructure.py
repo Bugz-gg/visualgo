4adbdfa5d1057d169fddf25877d25a641c27402b
YOURI CHANCRIN <Youri.Chancrin@bordeaux-inp.fr>
2024-02-28 09:57:20 +0100

    POC v1

--- src/visualgo/visu/displayDataStructure.py
+++ src/visualgo/visu/displayDataStructure.py
@@ -1,42 +1,58 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QVBoxLayout, QHBoxLayout
-from PyQt5.QtCore import Qt, QRect 
-from PyQt5.QtGui import QPainter, QPen
+import random
+
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QVBoxLayout, QHBoxLayout, QPushButton
+from PyQt5.QtCore import Qt, QRect
+from PyQt5.QtGui import QPainter, QPen, QColor
+
 
 class ArrayCellWidget(QWidget):
     def __init__(self, value):
         super().__init__()
-        # set value
         self._value = value
-        self.setObjectName("ArrayCellWidget")  # Set object name for styling
+        self.is_selected = False
+
+    def toggle_selection(self):
+        self.is_selected = not self.is_selected
+        self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        
-        # Draw the rectangle with the cell value in it
-        painter.drawText(QRect(0, 0, self.width(), self.height()), Qt.AlignCenter, str(self._value))
-    
+        font = painter.font()
+        font.setPointSize(20)
+        painter.setFont(font)
+
+        if self.is_selected:
+            painter.fillRect(self.rect(), QColor("lightblue"))
+        painter.drawText(self.rect(), Qt.AlignCenter, str(self._value))
+
+
 class ArrayWidget(QWidget):
     def __init__(self, array):
         super().__init__()
-        self.array = array
-        self.setObjectName("ArrayCellWidget")  # Set object name for styling
+        self.cell_array = [ArrayCellWidget(value) for value in array]
+        self.setObjectName("ArrayWidget")  # Set object name for styling
 
         # Create a horizontal layout for the ArrayWidget
         layout = QHBoxLayout()
 
         # Create an ArrayCellWidget for each array value and add it to the layout
-        for  value in array:
-            cellWidget = ArrayCellWidget(value)
-            layout.addWidget(cellWidget)  
+        for cell in self.cell_array:
+            cellLayout = QVBoxLayout()
+            cellLayout.addWidget(cell)
+            selection_button = QPushButton("Select me!")
+            selection_button.clicked.connect(cell.toggle_selection)
+            cellLayout.addWidget(selection_button)
+            layout.addLayout(cellLayout)
 
-        # Set the layout on the ArrayWidget
+            # Set the layout on the ArrayWidget
         self.setLayout(layout)
+
 
 if __name__ == "__main__":
     app = QApplication([])
     # load QSS file
     with open("style.qss", "r") as f:
         app.setStyleSheet(f.read())
-    window = ArrayWidget([1,2,3,4])
+    window = ArrayWidget([1, 2, 3, 4])
     window.showMaximized()
     app.exec()
--- src/visualgo/visu/mainWindow.py
+++ src/visualgo/visu/mainWindow.py
@@ -1,32 +1,47 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QGroupBox, QHBoxLayout, QWidget
+
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QGroupBox, QHBoxLayout, QWidget, QPushButton, \
+    QVBoxLayout, QLabel
 from displayDataStructure import ArrayWidget
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Visualgo")
 
-        # Create a QTextEdit widget
-        self.textEdit = QTextEdit()
+        self.array_widget = ArrayWidget([1, 2, 3, 4, 5])
 
-        # Create an ArrayWidget
-        self.ArrayWidget = ArrayWidget([1,2,3,4,5])
+        layout = QVBoxLayout()
+        title = QLabel("Test de visualisation")
+        title_font = QFont()
+        title_font.setPointSize(20)
+        title.setFont(title_font)
+        title.setAlignment(Qt.AlignCenter)
+        layout.addWidget(title)
 
-        # Create a horizontal layout for the main window with code left and array right
-        layout = QHBoxLayout()
+        layout.addWidget(self.array_widget)
+        controller_layout = QHBoxLayout()
 
-        # Add the widgets to the layout
-        layout.addWidget(self.textEdit)
-        layout.addWidget(self.ArrayWidget)
+        run_button = QPushButton("Run")
 
-        # Make the array takes 3/4 of the window and the code takes 1/4
-        layout.setStretch(0, 1)
-        layout.setStretch(1, 3)
+        controller_layout.addWidget(run_button)
+        controller_layout.addWidget(QPushButton("Step"))
+        layout.addLayout(controller_layout)
+
         # Create a central widget for the QMainWindow and set the layout
         centralWidget = QWidget()
         centralWidget.setLayout(layout)
         self.setCentralWidget(centralWidget)
+
+        layout.setStretch(0, 1)
+        layout.setStretch(1, 3)
+        layout.setStretch(2, 1)
+
+        self.setMinimumSize(QSize(600, 300))
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -35,5 +50,5 @@ if __name__ == "__main__":
         app.setStyleSheet(f.read())
 
     window = MainWindow()
-    window.showMaximized()
+    window.show()
     sys.exit(app.exec())
