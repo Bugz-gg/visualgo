cbb27d24766b700a7159a669a8fe7e2df33a6455
Ilyes Bechoual <ilyes.bechoual@bordeaux-inp.fr>
2024-03-05 14:22:44 +0100

    added listadrr and a method around it to stock the different value through a code of an attribut

--- src/visualgo/flow_control/test_flow_control.py
+++ src/visualgo/flow_control/test_flow_control.py
@@ -1,15 +1,19 @@
 from re import *
 from typing import Any
+# from PySide2.QtCore import QObject, Signal
+import time
+
 
 class program:
     pass
 
+
 class program2:
     def __init__(self, program):
-        self.program=program
-    
+        self.program = program
+
     def spliting(self):
-        self.split_program=self.program.split("\n")
+        self.split_program = self.program.split("\n")
 
     def execute(self):
         exec(self.program)
@@ -22,17 +26,23 @@ class program2:
     def run(self):
         self.execute()
 
+
 class program4:
     def __init__(self, program):
-        self.program=program
+        self.program = program
 
     def spliting_lines(self):
-        self.split_program=self.program.split("\n")
-    
+        self.split_program = self.program.split("\n")
+
     def concat_lines(self):
         self.compiled = ""
         for line in self.compiled_program:
             self.compiled += line + "\n"
+
+    def lock(self):
+        print("Type c to continue execution...")
+        while input() != 'c':
+            time.sleep(1)
 
     def compile(self):
         # Dictionary to store variable assignments
@@ -52,68 +62,91 @@ class program4:
                 compiled_line = f"self.{lhs} = {rhs}"
                 variables[lhs] = f"self.{lhs}"
             else:
-                # Other lines, just replace variables
-                for var, repl in variables.items():
-                    line = line.replace(var, repl)
-                compiled_line = line
+                # We can do a control_func_list and then do if line in control_func_list.
+                if line == 'lock()':
+                    self.lock()
+                    compiled_line = line
+                else:
+                    # Other lines, just replace variables
+                    for var, repl in variables.items():
+                        line = line.replace(var, repl)
+                    compiled_line = line
             self.compiled_program.append(compiled_line)
-            #print(self.compiled_program)
-    
+            print(self.compiled_program)
+
     def execute(self):
         exec(self.compiled)
-    
+
     def run(self):
         self.spliting_lines()
 
         self.compile()
 
         self.concat_lines()
-        #print(self.compiled)
+        # print(self.compiled)
 
         self.execute()
 
 
-
 class attribute:
-    def __init__(self, value: Any, VISUALIZE=False):
-        self.value=value
-        self.VISUALIZE=VISUALIZE
-        self.count=0
-    
+    def __init__(self, value: Any, name: str, count=0, VISUALIZE=False, line=0):
+        self.value = value
+        self.name = name
+        self.VISUALIZE = VISUALIZE
+        self.count = count
+        self.code_line = line
+
     def __str__(self):
         return str(self.value)
-    
+
     def __add__(self, other):
         return attribute(self.value + other.value, self.VISUALIZE)
-    
+
     def __mul__(self, other):
         if isinstance(other, attribute):
             return attribute(self.value * other.value, self.VISUALIZE)
         elif isinstance(other, int) or isinstance(other, float):
             return attribute(self.value * other, self.VISUALIZE)
         else:
-            raise TypeError("Unsupported operand type for *: {}".format(type(other)))
-    
+            raise TypeError(
+                "Unsupported operand type for *: {}".format(type(other)))
+
     def debug(self):
         print("\n[DEBUG]", type(self).__name__)
+        print("Name of attribute ", self.name)
         print("value\t\t", self.value)
         print("VISUALIZE\t", self.VISUALIZE)
-        print("count\t\t", self.count, "\n")
+        print("count\t\t", self.count)
+        print("line of code\t", self.code_line, "\n")
 
 
 class program3:
+    __listattr = {}
+
     def __init__(self, clock=0):
         self.clock = clock
 
     def __setattr__(self, __name: str, __value: Any) -> None:
-        self.__dict__[__name] = attribute(__value)
-    
+        if __name not in self.__listattr:
+            self.__dict__[__name] = attribute(__value, __name, 0)
+            self.__listattr[__name] = {self.__dict__[
+                __name].count: self.__dict__[__name]}
+        else:
+            self.__dict__[__name] = attribute(
+                __value, __name, len(self.__listattr[__name]))
+            self.__listattr[__name][self.__dict__[
+                __name].count] = self.__dict__[__name]
+
     def __getattr__(self, name: str) -> Any:
         if name in self.__dict__:
             return self.__dict__[name].value
         else:
-            raise AttributeError(f"'program3' object has no attribute '{name}'")
-    
+            raise AttributeError(
+                f"'program3' object has no attribute '{name}'")
+
+    def historic(self, variable: str) -> dict:
+        return self.__listattr[variable]
+
     def debug(self):
         print("\n[DEBUG]", type(self).__name__)
         for attr in self.__dict__:
--- tests/test_flow_control.py
+++ tests/test_flow_control.py
@@ -1,4 +1,5 @@
-import visualgo.flow_control.test_flow_control as vfc 
+import visualgo.flow_control.test_flow_control as vfc
+
 
 def test_class_1():
     print("PROGRAM TEST :\n")
@@ -14,6 +15,7 @@ def test_class_1():
     print("#=======================================================================================================#")
     return 0
 
+
 def test_class_2():
     print("PROGRAM2 TEST :\n")
     prog = """
@@ -27,9 +29,10 @@ f(n)
     """
     test = vfc.program2(prog)
     test.run()
-    #test.execute_line_by_line()
+    # test.execute_line_by_line()
     print("#=======================================================================================================#")
     return 0
+
 
 def test_class_4():
     prog = """
@@ -39,7 +42,7 @@ print(x)"""
     print("PROGRAM4 TEST :", prog, "\n")
     test = vfc.program4(prog)
     test.run()
-    prog2="""
+    prog2 = """
 def f(x):
     print(x)
     x <- 8
@@ -52,8 +55,14 @@ f(x)
     # name 'f' is not defined -> why ?
     test2 = vfc.program4(prog2)
     test2.run()
-    #need to keep in mind all \t (tabs) for block indent
+    # need to keep in mind all \t (tabs) for block indent
+    prog3 = """
+self.lock()
+    """
+    test3 = vfc.program4(prog3)
+    test3.run()
     print("#=======================================================================================================#")
+
 
 def test_class_3():
     print("PROGRAM3 TEST :\n")
@@ -68,12 +77,17 @@ def test_class_3():
     print("k = k * x :", test.k)
     test.k.debug()
     test.debug()
+    u = test.historic("k")
+    for value in u:
+        print(f"value : {u[value].value}, name : {u[value].name}")
+
     # Breakpoint
     #   python debugger (pdb library) breakpoint
     #   pause() or wait_input()
 
     # Pseudo-code
     #   compilator -> compile from string to string / blocks are indented
+
 
 if __name__ == "__main__":
     test_class_1()
