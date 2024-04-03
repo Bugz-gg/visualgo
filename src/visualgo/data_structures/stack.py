f2ab8c1576f6566178acf6b574e29f5fe0937fd8
tdaponte <tanguy.daponte@gmail.com>
2024-04-03 13:06:42 +0200

    fix: fixing Stack status change and adding peep method

--- src/visualgo/data_structures/stack.py
+++ src/visualgo/data_structures/stack.py
@@ -1,35 +1,42 @@
 from visualgo.data_structures.data import Data,Status
+import traceback
+
 
 class Stack(Data):
-    def __init__(self, is_visualisable):
-        self.data = []
-        self.is_visualisable = is_visualisable
+    def __init__(self, ):
+        super().__init__()
+        self.value = []
     
     def pop(self):
         assert not self.isEmpty(), "stack is empty"
-        return self.data.pop(0)
+        return self.value.pop(0)
     
     def push(self, value):
-        self.data.insert(0, value) 
+        self.value.insert(0, value) 
         assert not self.isEmpty(), "operation failed, stack is empty"
         
     def isEmpty(self):
         return self.size() == 0
     
     def size(self):
-        return len(self.data)
+        return len(self.value)
     
     def clear(self):
-        self.data = []
+        self.value = []
         assert self.isEmpty(), "stack not empty"
     
     def affect(self, stack):
         assert isinstance(stack, Stack), "r value must but a Stack"
-        self.data = stack.data
+        self.value = stack.data
         self.status = Status.AFFECTED
-        
-        
     
+    def peep(self):
+        assert not self.isEmpty(), "stack is empty"
+        return self.value[0]
     
-    
+    def __eq__(self, other):
+        super().set_status(Status.COMPARED)
+        if isinstance(other, Stack):
+            return self.value == other.value
+        return False
     
--- tests/data_structures/test_stack.py
+++ tests/data_structures/test_stack.py
@@ -1,23 +1,51 @@
 from visualgo.data_structures.stack import Stack
+from visualgo.data_structures.number import Status
+
 
 def test_stack_init():
-    stack = Stack(True)
-    assert stack.isEmpty() == True
+    stack = Stack()
+    assert stack.isEmpty()
     
 def test_stack_push():
-    stack = Stack(True)
+    stack = Stack()
     stack.push(1)
     assert stack.isEmpty() == False
     assert stack.size() == 1
-    assert stack.data[0] == 1
+    assert stack.value[0] == 1
 
 def test_stack_pop():
-    stack = Stack(True)
+    stack = Stack()
     # stack.Pop() # test error case
-    assert stack.isEmpty() == True
+    assert stack.isEmpty()
     
     stack.push(1)
     value = stack.pop()
-    print(stack.data)
     assert value == 1    
+    
+def test_stack_peep():
+    stack = Stack()
+    assert stack.isEmpty()
+
+    stack.push(42)
+    assert stack.isEmpty() != True
+    assert stack.peep() == 42
+
+# --------------- Status tests ---------------
+
+def test_number_status_created():
+    s = Stack()
+    assert s.get_status() == Status.CREATED
+    
+def test_number_affected():
+    s = Stack()
+    s.value = []
+    assert s.get_status() == Status.AFFECTED
+
+def test_number_compared():
+    s1 = Stack()
+    s2 = Stack()
+    s1 == s2
+    assert s1.get_status() == Status.COMPARED
+
+
     
