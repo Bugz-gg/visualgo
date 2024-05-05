fd45baf0f282bdbc8f93a423316b6bc6d2776e12
Alexandre Tabouret <Alexandre.Tabouret@bordeaux-inp.fr>
2024-05-05 10:43:36 +0200

    added commentary for stacka and queue

--- src/visualgo/data_structures/queue.py
+++ src/visualgo/data_structures/queue.py
@@ -6,22 +6,48 @@ class Queue(Data):
         self.value = []
     
     def remove(self):
+        """
+        Removes the first element of the queue. Assert that the queue is not empty.
+
+        :return: The first element of the queue.
+        """
         super().set_status(Status.READ)
         assert not self.isEmpty(), "queue is empty"
         return self.value.pop(self.size() - 1)
     
     def add(self, value):
+        """
+        Adds a value at the end of the queue.
+        
+        :param value: Value to add at the end of the queue.
+        :return:
+        """
         super().set_status(Status.READ)
         self.value.insert(0, value) 
         assert not self.isEmpty(), "operation failed, queue is empty"
         
     def isEmpty(self):
+        """
+        Checks if the queue is empty.
+
+        :return: A boolean, True if the queue is empty, False otherwise.
+        """
         return self.size() == 0
     
     def size(self):
+        """
+        Returns the number of elements in the queue.
+
+        :return: The number of elements in the queue.
+        """
         return len(self.value)
     
     def clear(self):
+        """
+        Reset the queue by removing all elements.
+
+        :return:
+        """
         self.value = []
         assert self.isEmpty(), "queue not empty"
     
--- src/visualgo/data_structures/stack.py
+++ src/visualgo/data_structures/stack.py
@@ -10,11 +10,22 @@ class Stack(Data):
         return ", ".join(str(val) for val in self.__dict__['value']) + f" {self.status}"
     
     def pop(self):
+        """
+        Removes the top element of the stack. Assert that the stack is not empty.
+
+        :return: The top element of the stack.
+        """
         super().set_status(Status.READ)
         assert not self.isEmpty(), "stack is empty"
         return self.value.pop(0)
     
     def push(self, value):
+        """
+        Pushes a value onto the top of the stack.
+        
+        :param value: Value to push onto the stack.
+        :return:
+        """
         super().set_status(Status.READ)
         if isinstance(value, int):
             self.value.insert(0, Number(value))
@@ -22,16 +33,36 @@ class Stack(Data):
             self.value.insert(0, value)
         
     def isEmpty(self):
+        """
+        Checks if the stack is empty.
+
+        :return: A boolean, True if the stack is empty, False otherwise.
+        """
         return self.size() == 0
 
     def size(self):
+        """
+        Returns the number of elements in the stack.
+
+        :return: The number of elements in the stack.
+        """
         return len(self.value)
     
     def clear(self):
+        """
+        Reset the stack by removing all elements.
+
+        :return:
+        """
         self.value = []
         assert self.isEmpty(), "stack not empty"
     
     def peep(self):
+        """
+        Returns the top element of the stack without removing it. Assert that the stack is not empty.
+
+        :return: The top element of the stack.
+        """
         assert not self.isEmpty(), "stack is empty"
         return self.value[0]
     
